a005603045846bb5d9a2abbdd81555b8524591d2
PhillipCostas <Phillipcostas@gmail.com>
2024-07-18 13:35:44 -0400

    another commit

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 class Game():
-    def __init__(self, turn):
+    def __init__(self, turn='X'):
         self.turn = turn
         self.board = {
             'a1': None, 'b1': None, 'c1': None,
@@ -8,7 +8,7 @@ class Game():
         }
         self.winner = None
         self.tie = False
-    
+
     def print_board(self):
         b = self.board
         print(f"""
@@ -20,7 +20,6 @@ class Game():
     3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
   """)
 
-
     def print_message(self):
         if self.tie:
             print("Tie game!")
@@ -28,20 +27,55 @@ class Game():
             print(f"{self.winner} wins the game!")
         else:
             print(f"It's player {self.turn}'s turn!")
-        
+
     def render(self):
-        game.print_board()
-        game.print_message()
+        self.print_board()
+        self.print_message()
 
-    def get_move(self):
-        move = input(f'Enter a valid move (example: A1:): ').lower()
-        print(f'The user entered {move}')
+    def get_valid_move(self):
+        while True:
+            move = input("Enter your move (e.g., a1, b2): ").lower()
+            if move in self.board and self.board[move] is None:
+                return move
+            else:
+                print("Invalid move. Please try again.")
 
-        while True: 
-            move = input 
+    def place_piece(self):
+        move = self.get_valid_move()
+        self.board[move] = self.turn
+        self.check_winner() 
+        self.check_tie()  
+        self.turn = 'O' if self.turn == 'X' else 'X'
+
+    def check_winner(self):
+        pass
+
+    def check_tie(self):
+        pass
+
+    def check_for_winner(self):
+        self.board['a1'] and (self.board['a1'] == self.board['b1'] == self.board['c1'])
+
+        self.board['a2'] and (self.board['a2'] == self.board['b2'] == self.board['c2'])
+
+        self.board['a3'] and (self.board['a3'] == self.board['b3'] == self.board['c3'])
+
+        self.board['a1'] and (self.board['a1'] == self.board['b2'] == self.board['c3'])
+
+        self.board['a1'] and (self.board['a1'] == self.board['b1'] == self.board['c1'])
+
+        self.board['a1'] and (self.board['a1'] == self.board['b1'] == self.board['c1'])
+
+        self.board['a1'] and (self.board['a1'] == self.board['b1'] == self.board['c1'])
+        
+        self.board['a1'] and (self.board['a1'] == self.board['b1'] == self.board['c1'])
 
 
-game = Game('X')
-game.print_board()
-game.print_message()
-game.get_move()
+    print(check_for_winner)
+
+
+game = Game()
+while not game.winner and not game.tie:
+    game.render()
+    game.place_piece()
+game.render() 
